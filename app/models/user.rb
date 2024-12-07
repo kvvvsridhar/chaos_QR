20e4fe59708372fd7a06a4e5227532792f1f116e
kvvvsridhar <kvvvsridhar@gmail.com>
2024-12-07 10:40:33 +0530

    id_number validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+    validates :id_number, presence: true, uniqueness: true
+    validate :id_number_exists, on: :update # Validation runs during update
+
+  private
+
+  def id_number_exists
+    unless User.exists?(id_number: id_number)
+      errors.add(:id_number, "is not valid. Please scan a QR code of a registered user.")
+    end
+  end
 end
